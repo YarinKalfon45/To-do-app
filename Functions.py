25a76919c5975a0a659c2b4437ed08f275963122
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-19 20:03:19 +0200

    Documentation

--- Functions.py
+++ Functions.py
@@ -1,7 +1,15 @@
+"""
+Functions File
+"""
+
 from datetime import datetime
 
 
 def showtime():
+    """
+    A function that shows current time for GUI version
+    :return: current time and date
+    """
     now = datetime.now()
     date_str = now.strftime("%d/%m/%Y")
     time_str = now.strftime('%H:%M:%S')
@@ -10,12 +18,22 @@ def showtime():
 
 
 def get_lst(filepath):
+    """
+    A function that returns the list of our tasks
+    :param filepath: txt file with all the tasks
+    :return: list with all the tasks
+    """
     with open(filepath, "r") as file:
-        lst = file.readlines()
+        lst = file.readlines()  # Each line turns into element in the list
         return lst
 
 
 def show_list(filepath):
+    """
+    This function Shows the list of tasks
+    :param filepath: txt file with all the tasks
+    :return:
+    """
     print("Your tasks for today are:")
     lst = get_lst(filepath)
     for i in range(len(lst)):
@@ -24,32 +42,54 @@ def show_list(filepath):
 
 
 def add_task(filepath, task):
+    """
+    A function that adds a new task
+    :param filepath: txt file with all the tasks
+    :param task: the task wich we want to add
+    :return:
+    """
     with open(filepath, "a") as file:
-        file.write(task.capitalize() + '\n')
+        file.write(task.capitalize() + '\n')  # Writing The new task at the end of the file
 
 
-def edit_task(filepath,old_task,new_task):
+def edit_task(filepath, old_task, new_task):
+    """
+    A function that edits an exiting task
+    :param filepath: txt file with all the tasks
+    :param old_task: the task we want to edit
+    :param new_task: the new task to replace it
+    :return:
+    """
     lst = []
-    lst = get_lst(filepath)
-    index = lst.index(old_task)
-    new_task = new_task +'\n'
-    lst[index] = new_task
+    lst = get_lst(filepath)  # Getting the list
+    index = lst.index(old_task)  # Finding the index of the task we want to change
+    new_task = new_task + '\n'  # Adding '/n' so the file format stays the same
+    lst[index] = new_task  # Replacing the old task with the edited task
     with open(filepath, "w") as file:
         for item in lst:
-            file.write(item.capitalize())
+            file.write(item.capitalize())  # Writing the new list into the file
 
 
 def complete(filepath, task_to_complete):
-        lst = get_lst(filepath)
-        index = lst.index(task_to_complete)
-        lst.pop(index)
-        with open(filepath, "w") as file:
-            for item in lst:
-                file.write(item.capitalize())
-
+    """
+    A function that enables to mark task as completed an remove it from the tasks list
+    :param filepath: txt file with all the tasks
+    :param task_to_complete: the task we completed
+    :return:
+    """
+    lst = get_lst(filepath)  # Getting the list
+    index = lst.index(task_to_complete)   # Finding the index of the task we want to remove
+    lst.pop(index)  # Removing the task
+    with open(filepath, "w") as file:
+        for item in lst:
+            file.write(item.capitalize())  # Writing the updated list to the file
 
 
 def menu():
+    """
+    This is a CLI version to check the functions
+    :return:
+    """
     filepath = "tasks.txt"
     while True:
         print("Hello, please select desired action:")
--- web.py
+++ web.py
@@ -1,24 +1,33 @@
+
+
 import streamlit as st
 import Functions
 st.set_page_config(layout="wide")
-filepath = 'tasks.txt'
+filepath = 'tasks.txt'  # Default filepath
 lst = Functions.get_lst(filepath)
 
 
 def add_task():
-    task = st.session_state["new_task"]
-    Functions.add_task(filepath, task)
-    st.session_state["new_task"] = ""
+    """
+    This is a function for the web app that adds new task
+    :return:
+    """
+    task = st.session_state["new_task"]  # Saving the task the user inputted
+    Functions.add_task(filepath, task)  # Using our function to add a new task for the list
+    st.session_state["new_task"] = ""  # Clearing the input box
 
 st.title("My To-Do App")
 st.subheader("this is my to-do app")
 st.write("This app is to increase your <b>productivity!</b>",unsafe_allow_html=True)
 st.text_input(label="", placeholder="Add new task...", on_change=add_task, key="new_task")
 
-for item in lst:
+"""
+This next loop job is to remove checked tasks from the to do list
+"""
+for item in lst:  # Iterating over every element in the list
     checkbox = st.checkbox(item,key=item)
-    if checkbox:
-        Functions.complete(filepath,item)
+    if checkbox:  # If checkbox in checked
+        Functions.complete(filepath,item)  # Using our complete function
         del st.session_state[item]
         st.experimental_rerun()
 
